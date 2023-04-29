3d3063d73224d2be09e8c54d6433fe35b583525b
vikash kumar <vkvikashkumar987@gmail.com>
2023-04-29 22:20:08 +0530

    create user,addFodItem,getAllFoodIDs

---
+++ data.js
@@ -0,0 +1,77 @@
+const data = {
+     "users": [
+          {
+               "user_id": "1",
+               "food_ids": [
+                    {
+                         "food_id": "1",
+                         "name": "Pizza",
+                         "ingredients": ["dough", "tomato sauce", "cheese", "toppings"],
+                         "calories": 285,
+                         "protein": 12,
+                         "carbs": 36,
+                         "fat": 10
+                    },
+                    {
+                         "food_id": "2",
+                         "name": "Salad",
+                         "ingredients": ["lettuce", "cucumber", "tomato", "dressing"],
+                         "calories": 150,
+                         "protein": 4,
+                         "carbs": 10,
+                         "fat": 8
+                    },]
+          }
+     ]
+}
+
+// {
+//      "users": [
+//        {
+//          "user_id": "user1",
+//          "food_ids": [
+//            {
+//              "food_id": "food1",
+//              "name": "Pizza",
+//              "ingredients": ["dough", "tomato sauce", "cheese", "toppings"],
+//              "calories": 285,
+//              "protein": 12,
+//              "carbs": 36,
+//              "fat": 10
+//            },
+//            {
+//              "food_id": "food2",
+//              "name": "Salad",
+//              "ingredients": ["lettuce", "cucumber", "tomato", "dressing"],
+//              "calories": 150,
+//              "protein": 4,
+//              "carbs": 10,
+//              "fat": 8
+//            }
+//          ]
+//        },
+//        {
+//          "user_id": "user2",
+//          "food_ids": [
+//            {
+//              "food_id": "food3",
+//              "name": "Burger",
+//              "ingredients": ["bun", "patty", "cheese", "lettuce", "tomato", "sauce"],
+//              "calories": 450,
+//              "protein": 18,
+//              "carbs": 40,
+//              "fat": 25
+//            },
+//            {
+//              "food_id": "food4",
+//              "name": "Pasta",
+//              "ingredients": ["noodles", "tomato sauce", "meat", "cheese"],
+//              "calories": 350,
+//              "protein": 15,
+//              "carbs": 50,
+//              "fat": 12
+//            }
+//          ]
+//        }
+//      ]
+//    }
